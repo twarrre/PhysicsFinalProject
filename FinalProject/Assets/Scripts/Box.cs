00a3789dda23bd2d34fc42021502a0d9d66b7892
twarrre <tw4194@hotmail.com>
2015-04-18 11:22:20 -0700

    Finished the box class and made prefabs.

--- FinalProject/Assets/Scripts/Box.cs
+++ FinalProject/Assets/Scripts/Box.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Box : MonoBehaviour {
 
-    public const int WIDTH = 100;
+    public const int WIDTH  = 100;
     public const int HEIGHT = 100;
 
     public int mass;
@@ -15,9 +15,21 @@ public class Box : MonoBehaviour {
     public Vector3 corner3;
     public Vector3 corner4;
 
+    public GameObject sphere1;
+    public GameObject sphere2;
+    public GameObject sphere3;
+    public GameObject sphere4;
+
+    public float e = 0.0f;
+
 	// Use this for initialization
 	void Start ()
     {
+        sphere1 = GameObject.Find("Sphere1");
+        sphere2 = GameObject.Find("Sphere2");
+        sphere3 = GameObject.Find("Sphere3");
+        sphere4 = GameObject.Find("Sphere4");
+
         mass = 0;
         velocity = corner1 = corner2 = corner3 = corner4 = new Vector3();
 
@@ -26,6 +38,9 @@ public class Box : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	
+        corner1 = sphere1.transform.position;
+        corner2 = sphere2.transform.position;
+        corner3 = sphere3.transform.position;
+        corner4 = sphere4.transform.position;
 	}
 }
